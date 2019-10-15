dff3999cd8a73fea7cc9faa4db8f68ad13c91699
Your Name <you@example.com>
2019-10-15 09:12:23 +0200

    druhy

--- src/data/models/teplota.py
+++ src/data/models/teplota.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Column, ForeignKey
-from sqlalchemy.types import Boolean, Integer, String, DateTime, TIME
+from sqlalchemy.types import Boolean, Integer, String, DateTime, TIME, Time
 
 from ..database import db
 from ..mixins import CRUDModel
@@ -11,4 +11,4 @@ class Teplota(CRUDModel):
     __table_args__ = {'sqlite_autoincrement': True}
     id = Column(Integer, primary_key=True )
     teplota = Column(Integer, nullable=False, index=True)
-    cas_insertu = Column(TIME)
+    cas_insertu = Column(DateTime)
